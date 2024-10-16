881a8446bc92afef9be330404958ffe7c41493a2
ErickJamesRubin <rubin.erick123@gmail.com>
2024-10-16 12:52:15 +0800

    Changes and debugging of all related applet 4 files

--- Applet4.js
+++ Applet4.js
@@ -1,16 +1,15 @@
-class StudentList {
+class GameList {
     constructor(dataUrl) {
         this.dataUrl = dataUrl;
         this.games = [];
         this.init();
-    }   
+    }
 
     async init() {
         await this.fetchData();
-        this.renderGameList(this.games);
+        this.renderGameList(this.games); 
         this.bindSearchEvent();
     }
-
 
     async fetchData() {
         try {
@@ -21,39 +20,38 @@ class StudentList {
         }
     }
 
-
     renderGameList(games) {
-        const gameListContainer = document.getElementById('gameList');
-        gameListContainer.innerHTML = games.map(game =>
-            `<button class="btn btn-primary" style="margin-top:15px; 
-                                                    width:25rem">
-                ${game.game_name} | ${game.game_release_year}
-            </button><br>`
-        ).join('');
-    }
+        const gameSearchListContainer = document.getElementById('gameSearchList');
+        gameSearchListContainer.innerHTML = '';  
 
+        games.forEach(game => {
+            gameSearchListContainer.innerHTML += `
+                <p>${game.game_name}</p>
+                <p class="fw-light">${game.game_genre}</p>
+                <p class="text-muted">${game.game_release_year}</p>
+                <hr>
+            `;
+        });
+    }
+    
     bindSearchEvent() {
         const gameSearchBar = document.getElementById('gameSearchBar');
-        const gameSearchListContainer = document.getElementById('gameSearchList');
 
         gameSearchBar.addEventListener('input', () => {
-            this.filterGames(gameSearchBar.value, gameSearchListContainer);
+            this.filterGames(gameSearchBar.value);
         });
 
-        this.renderGameList(this.games, gameSearchListContainer);
+        this.renderGameList(this.games);
     }
 
-    
-    filterGames(query, searchListContainer) {
+    filterGames(query) {
         const filteredGames = this.games.filter(game => {
-            return game.game_name.toLowerCase().includes(query.toLowerCase());
+            return game.game_name.toLowerCase().includes(query.toLowerCase()) ||
+                   (game.game_genre && game.game_genre.toLowerCase().includes(query.toLowerCase()));
         });
 
-        searchListContainer.innerHTML = '';
-
-        this.renderGameList(filteredGames, searchListContainer);
+        this.renderGameList(filteredGames);   
     }
-
 }
 
-const gameList = new GameList('applet-4.json');
+const gameList = new GameList('Applet4.json');
